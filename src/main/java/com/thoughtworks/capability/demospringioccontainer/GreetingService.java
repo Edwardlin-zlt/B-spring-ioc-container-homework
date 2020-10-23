984eb41dd8c4d966d1c4087a4167d9da9c78055e
lintao <lintao.zhang@thoughtworks.com>
2020-10-23 21:23:10 +0800

    [Lintao] feat: p2s2 finished

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -2,8 +2,11 @@ package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("+++++++++++++++++++++++GreetingService+++++++++++++++++++++++");
+    }
 
     String sayHi() {
         return "hello world";
