056ba2e97956c70facecbbab851a98d231115378
lintao <lintao.zhang@thoughtworks.com>
2020-10-23 20:46:46 +0800

    [Lintao] feat: p2s1 finished

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+
+@Scope(SCOPE_PROTOTYPE)
 @Service
 public class GreetingService {
+    public GreetingService() {
+        System.out.println("++++++++++++++GreetingService+++++++++++++++");
+    }
 
     String sayHi() {
         return "hello world";
