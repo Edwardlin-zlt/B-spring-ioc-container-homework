04cfd65964f5117432aa6ff5fcb4804089757d72
lintao <lintao.zhang@thoughtworks.com>
2020-10-24 21:10:35 +0800

    [Lintao] feat: p3s3

---
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/AppConfig.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.capability.demospringioccontainer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+
+}
